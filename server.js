2e978b202063e83e7549443b2aa9b1bb23a2c481
israel <israel.bejarano.a@gmail.com>
2019-03-09 17:27:34 +0100

    add ruta about

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public')); // esto sirve el contenido publico a la web
-/* 
+
+hbs.registerPartials(__dirname + '/views/parciales');
+
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-    let salida = {
-        nombre: 'Israel',
-        edad: 30,
-        url: req.url
-    }
-    res.send(salida);
-}); */
+    res.render('home', {
+        nombre: 'Israel Bejarano Álvarez',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+});
 
 app.listen(3000, () => {
     console.log('Escuchando en puerto 3000');
